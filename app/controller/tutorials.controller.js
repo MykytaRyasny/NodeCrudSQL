e7bbfe248d8d853dafdc3c8bb5dd6ea31c89b2ee
MykytaRyasny <MykytaRyasny+code@gmail.com>
2023-06-23 01:18:17 +0200

    space test

--- app/controller/tutorials.controller.js
+++ app/controller/tutorials.controller.js
@@ -31,7 +31,7 @@ exports.create = (req, res) => {
 };
 
 // Retrieve all Tutorials from the database.
-exports.findAll = (req, res) => {};
+exports.findAll = (req, res) => {}; 
 
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {};
